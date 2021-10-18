6eb587c9d6316dc239d4ab698b59896a1f621ee7
Thaer Jomhawi <thaer.fjomhawi@gmail.com>
2021-10-18 07:56:26 +0300

    CC6

--- linked-list/app/src/main/java/linked/list/LinkedList.java
+++ linked-list/app/src/main/java/linked/list/LinkedList.java
@@ -35,4 +35,52 @@ public class LinkedList<T> {
         result += "NULL";
         return  result;
     }
+
+    public void append(T newVal)  {
+        Node _tmp = new Node(newVal);
+        if (head == null) {
+            head = _tmp;
+        } else {
+            Node cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = _tmp;
+        }
+    }
+
+    public void insertBefore(T value, T newVal) {
+        Node _tmp = new Node(newVal);
+        if (head == null) {
+            head = _tmp;
+        } else if (head.val == value) {
+            insert(newVal);
+        } else {
+            Node cur = head;
+            while (cur.next != null) {
+                if (cur.next.val == value) {
+                    _tmp.next = cur.next;
+                    cur.next = _tmp;
+                    break;
+                }
+                cur = cur.next;
+            }
+        }
+    }
+
+    public void insertAfter(T value, T newVal) {
+        Node _tmp = new Node(newVal);
+        if (head == null) {
+            head = _tmp;
+        } else {
+            Node cur = head;
+            while (cur != null) {
+                if (cur.val == value) {
+                    _tmp.next = cur.next;
+                    cur.next = _tmp;
+                }
+                cur = cur.next;
+            }
+        }
+    }
 }
